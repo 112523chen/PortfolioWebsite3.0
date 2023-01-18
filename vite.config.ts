59751b700836e293680856ed9d99556754787dfe
Alex Chen <112623chen@gmail.com>
2023-01-18 18:56:25 -0500

    update pathing

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,6 @@ import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: "/PortfolioWebsiteV3/",
+  base: "/PortfolioWebsite3.0/",
   plugins: [react()],
 });
